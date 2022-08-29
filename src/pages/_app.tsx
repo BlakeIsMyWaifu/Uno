f935d9615d2f78487a869c22192a77b72663edf8
BlakeIsMyWaifu <33188727+BlakeIsMyWaifu@users.noreply.github.com>
2022-08-29 08:36:41 +0100

    🧑‍💻 General setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,23 @@
-// src/pages/_app.tsx
-import "../styles/globals.css";
-import { SessionProvider } from "next-auth/react";
-import type { AppType } from "next/dist/shared/lib/utils";
-import { trpc } from "../utils/trpc";
+import { MantineProvider } from '@mantine/core'
+import { SessionProvider } from 'next-auth/react'
+import type { AppProps } from 'next/app'
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return (
-    <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
-};
+import { trpc } from '../utils/trpc'
 
-export default trpc.withTRPC(MyApp);
+const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
+	return (
+		<SessionProvider session={pageProps.session}>
+			<MantineProvider
+				withGlobalStyles
+				withNormalizeCSS
+				theme={{
+					colorScheme: 'dark'
+				}}
+			>
+				<Component {...pageProps} />
+			</MantineProvider>
+		</SessionProvider>
+	)
+}
+
+export default trpc.withTRPC(MyApp)
