f777bacf892c4e7dd06e95bdacd93f955aa1174f
Ramleton <77259553+Ramleton@users.noreply.github.com>
2022-08-31 05:17:07 -0400

    💥 Broken lobby creation

--- src/pages/lobby/[...id].tsx
+++ src/pages/lobby/[...id].tsx
@@ -1,6 +1,7 @@
 import { Box, Text } from '@mantine/core'
 import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
+import {useEffect} from 'react'
 import { trpc } from 'utils/trpc'
 
 const Lobby: NextPage = () => {
@@ -8,10 +9,14 @@ const Lobby: NextPage = () => {
 	const { query } = useRouter()
 	const test = trpc.proxy.lobby.createRoom.useQuery()
 
+	useEffect(() => {
+		console.log(test)
+	}, [test])
+
 	return (
 		<Box>
 			<Text>{query.id}</Text>
-			<Text>{test.data}</Text>
+			<Text>{test.data ?? 'ERROR!'}</Text>
 		</Box>
 	)
 }
--- src/server/trpc/router/lobby.ts
+++ src/server/trpc/router/lobby.ts
@@ -1,8 +1,32 @@
-import { t } from '../utils/trpc'
+import { authedProcedure, t } from '../utils/trpc'
 
 export const lobbyRouter = t.router({
-	createRoom: t.procedure
-		.query(async () => {
-			return await prisma?.user.count()
+	createRoom: authedProcedure
+		.query(async ({ ctx }) => {
+			const lobby = await ctx.prisma.lobby.create({
+				data: {}
+			})
+			const host = await ctx.prisma.player.create({
+				data: {
+					userId: ctx.session.user.id,
+					lobbyId: lobby.id,
+					isHost: true
+				}
+			})
+			await ctx.prisma.lobby.update({
+				where: {
+					id: lobby.id
+				},
+				data: {
+					players: {
+						set: [host]
+					}
+				}
+			})
+
+			console.log(lobby)
+			console.log(host)
+
+			return lobby.id
 		})
 })
